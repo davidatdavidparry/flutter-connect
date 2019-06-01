068dda8d60c1758b0ffe996073e6eac785c64176
David Parry <david.parry@firstaware.com>
2019-06-01 16:29:13 -0500

    pulling out the data while in the functional call

--- lib/ProfileWidget.dart
+++ lib/ProfileWidget.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:github/server.dart';
@@ -12,7 +11,13 @@ class ProfileWidget extends StatelessWidget {
 
   GitHub createClient() {
     if (github == null) {
-      github = GitHub(auth: Authentication.withToken("dae75dc5aadbb6dda2d30080ff3af34c0112597e"));
+      try {
+        github = GitHub(
+            auth: Authentication.withToken(
+                "317ec63f8e11acca1f9ddf36eebaa3d97e3b208b"));
+      } catch (exception, stackTrace) {
+        print("Excpetion occured ${exception}");
+      }
     }
     return github;
   }
@@ -64,51 +69,57 @@ class ProfileWidget extends StatelessWidget {
   }
 
   List<Widget> _getDataProjectUsers(AsyncSnapshot snapshot) {
-   List<User> users = snapshot.data;
-
+    List<String> users = snapshot.data;
+    List<Widget> widgets = new List();
     print('usrsCnt : $users');
-
-    List<Widget> wid = <Widget>[
-      Text(
-        'BIO data is, $users !',
-        textAlign: TextAlign.center,
-        style: TextStyle(fontWeight: FontWeight.bold),
-      )
-    ];
-
-
-
-    List<Widget> widgets = <Widget>[
-    Image.network( users.elementAt(0).avatarUrl.toString(),
-    )];
+    if(users != null) {
+      widgets = <Widget>[
+        Image.network(
+          users
+              .elementAt(0),
+        )
+      ];
+    } else {
+      widgets = <Widget>[
+        Text('List null')
+      ];
+    }
     return widgets;
   }
 
-  Future<List<User>> search(_) async {
-    print('here: $_');
+  Future<List<String>> search(_) async {
+    List<String> users = new List();
+    List<String> searchItems = new List();
+    var github = createClient();
+    if(github != null) {
+      Stream<CodeSearchResults> resultsStream = createClient().search.code(
+        'github',
+        language: 'flutter',
+        perPage: 10,
+        pages: 1,
+      );
 
-    List<User> users = new List();
+      await for (var results in resultsStream) {
+        print('results: $results ');
+        for (CodeSearchItem item in results.items) {
+          var login = item.repository.owner.login;
+          var id = item.repository.owner.id.toString();
+          print('item: $item ');
+          print("adding user login id $login");
+          searchItems.add(login);
+        }
+      }
+    }
 
-    Map map = new Map<String, User>();
-    Stream<CodeSearchResults> resultsStream =
-        createClient().search.code(
-              'github',
-              language: 'flutter',
-              perPage: 10,
-              pages: 1,
-            );
-
-    int count = 0;
-    await for (var results in resultsStream) {
-      print('results: $results ');
-      for (CodeSearchItem item in results.items) {
-        var login = item.repository.owner.login;
-        var id = item.repository.owner.id.toString() ;
-        print('item: $item ');
-        //map[id] = item;
-        print("calling to a user $login");
-        User u =  await createClient().users.getUser(login);
-        users.add(u);
+    if(searchItems.isNotEmpty) {
+      var github = createClient();
+      if(github != null) {
+        Stream<User> usersStreams = await createClient().users.getUsers(
+            searchItems);
+        await for (var resultUser in usersStreams) {
+          print("adding user to the list $resultUser ");
+          users.add(resultUser.avatarUrl);
+        }
       }
     }
 
